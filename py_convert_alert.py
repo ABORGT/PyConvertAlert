dab22f31aa10bd366d884a4c3485e833398bf90a
Justin Palmer <jpavlav@gmail.com>
2018-11-03 10:32:42 -0500

    Defined internal helper methods and public convert_it method.

--- py_convert_alert.py
+++ py_convert_alert.py
@@ -67,3 +67,175 @@ class Converter(object):
             raise TypeError("mapper must be dict {}==>{}".format(self._mapper,
                                                                  type(self._mapper)))
         return self._mapper
+
+    def _verify_key_exists(self, key, lookup_dict):
+        """
+        Return True if the specified key exists in our lookup_dict.
+
+        This is a protected method and should not be used outside of the public
+        convert_it method. This method is responsible for checking to see if we
+        get a hit on a specified key in a lookup on our dict. This helps us
+        ensure that during conversion, if any labels annotations or otherwise
+        will be unaffected by the conversion and thusly not return empty lists.
+
+        Parameters
+        ----------
+        key : str
+            The key we are looking up in lookup_dict.
+
+        lookup_dict : str
+            The lookup_dict is in our case an alert from an arbitrary alerting
+            system to be converted to an alert that Alert manager will accept.
+
+
+        Returns
+        -------
+        exists : boolean
+            Simple flag, True or False, to let us know if our search was
+            successful.
+
+
+        """
+        exists = False
+        if get_occurrence_of_key(lookup_dict, key) > 0:
+            exists = True
+        return exists
+
+    def _key_list_search(self, keys_list, lookup_dict):
+        """
+        Return the final value returned after iterating over keys_list.
+
+        In order to handle situations where a key might exist twice in the
+        alert on which we run our lookup, we can specify a list of keys to
+        iterate over. Essentially, if a list is provided as the value to one of
+        our corresponding alert manager alert keys, we will search for the
+        first key in the list, return it's value, then search for the next key
+        etc. Finally when we reach the end of our list, the value returned
+        should be the result of all searches. This is a protected method and
+        should only be used in the public convert_it method.
+
+        Parameters
+        ----------
+        keys_list : list of str
+            This is the list of keys to drill down on. It will be iterated over
+            until we reach the end of the list, providing the more specific key
+            lookup to prevent multiple values for a single lookup.
+
+        lookup_dict : dict
+            This is the arbitrary alert that we are searching through for keys
+            to add to our alert manager alert.
+
+
+        Returns
+        -------
+        value : str
+            This value should be the result of all of the searches for the keys
+            provided.
+
+        """
+        for index, key in enumerate(keys_list):
+            result = nested_lookup(key, lookup_dict)
+            try:
+                value = nested_lookup(keys_list[index + 1], result)
+            except IndexError:
+                pass
+        return value
+
+    def _add_found_values(self, transform_dict, transform_key,
+                          lookup_key, lookup_dict):
+        """
+        Set value for the specified key in our transform dict to our lookup.
+
+        This is a protected method that shouldn't be used outside of the
+        public convert_it method. This method is responsible for all of our
+        lookups and assigning the corresponding found value to the appropriate
+        key in our alert manager alert.
+
+        Parameters
+        ----------
+        transform_dict : dict
+            The transform_dict is our self.mapping values. The dict is
+            transformed by the iterating over it in the convert_it method and
+            adding values here.
+
+        transform_key : str
+            This is the key we want to target in our transform dict. Ultimately
+            the value returne from the lookup will be assigned as the value of
+            this key.
+
+        lookup_key : str
+            This is the key we are looking up in our arbitrary alert json
+            structure to be assigned as the value of the transform_key.
+
+        lookup_dict : dict
+            This is the arbitrary alert we are searching for values to be
+            assigned to our transform_dict.
+
+
+        Returns
+        -------
+        transform_dict : dict
+            Return the modified transform dict back to the convert_it method.
+            This transform dict should now have the keys we specified in our
+            mapper. There corresponding values should now be changed to the
+            result of searching for what was the key's previous value (our
+            lookup_key is replaced by an actual value).
+
+        """
+        try:
+            if self._verify_key_exists(lookup_key, lookup_dict):
+                transform_dict[transform_key] = \
+                    ''.join(nested_lookup(lookup_key, lookup_dict))
+        except TypeError:
+            pass
+        if isinstance(lookup_key, list):
+            transform_dict[transform_key] = \
+                ''.join(self._key_list_search(lookup_key, lookup_dict))
+        return transform_dict
+
+    def _map_to_mapper(self, convert_alert, alert):
+        for key, value in convert_alert.items():
+            if isinstance(value, dict):
+                for k, v in value.items():
+                    value = self._add_found_values(value, k, v, alert)
+        return value
+
+    def convert_it(self, alert):
+        """
+        Return either a single converted alert or a list of them.
+
+        This method is responsible for returning our converted alerts. For
+        flexibility purposes, it will accept either a single arbitrary alert
+        or a list of alerts. If a single alert is provided, a single converted
+        alert will be returned. If a list of alerts are provided, a list of
+        converted alerts will be returned.
+
+        Parameters
+        ----------
+        alert : dict
+            alert is the alert originating from any arbitrary alerting system.
+
+
+        Returns
+        -------
+        ret : dict or list of dicts
+            ret is set dynamically based on whether or not a list of dicts is
+            passed to convert_it or a single dictionary. Either way, it returns
+            a dict where all keys that matched a corresponding key in the
+            alert.
+
+
+        """
+        converted_alert = copy.deepcopy(self.mapping)
+        converted_alert_list = list()
+
+        if isinstance(alert, list):
+            for single_alert in alert:
+                converted_alert = copy.deepcopy(self.mapping)
+                self._map_to_mapper(converted_alert, single_alert)
+                converted_alert_list.append(converted_alert)
+            ret = converted_alert_list
+        else:
+            self._map_to_mapper(converted_alert, alert)
+            ret = converted_alert
+        return ret
